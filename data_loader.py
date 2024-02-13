5f1aabe58af362c45feff89a203173b078dd1f7d
Alishba Imran <alishbai734@gmail.com>
2024-02-13 06:11:32 +0000

    check BRC errors

---
+++ data_loader.py
@@ -0,0 +1,53 @@
+import os
+
+import torch
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+
+def data_loader(root, batch_size=256, workers=1, pin_memory=True):
+    traindir = os.path.join(root, 'train')
+    valdir = os.path.join(root, 'val')
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+
+    train_dataset = datasets.ImageFolder(
+        traindir,
+        transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalize,
+        ])
+    )
+    val_dataset = datasets.ImageFolder(
+        valdir,
+        transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            normalize,
+        ])
+    )
+
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=workers,
+        pin_memory=pin_memory,
+        sampler=None
+    )
+    val_loader = torch.utils.data.DataLoader(
+        val_dataset,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=workers,
+        pin_memory=pin_memory
+    )
+
+    sample_train_image = next(iter(train_loader))[0][0]
+    sample_val_image = next(iter(val_loader))[0][0]
+    print("Shape of the first training image:", sample_train_image.shape)
+    print("Shape of the first validation image:", sample_val_image.shape)
+
+    return train_loader, val_loader
